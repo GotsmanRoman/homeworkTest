9e72a41a9eb7ffb0fc54b5fef8619862f4afc332
Gotsman Roman <gotsmanroman@gmail.com>
2023-05-19 22:49:06 +0300

    fix

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -3,73 +3,91 @@ import Loader from '../Loader/Loader';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import './ImageGallery.css';
 import Button from '../Button/Button';
-import fetchFoto from '../../api/api'
+import fetchFoto from '../../api/api';
 import Modal from '../Modal/Modal';
 
-export default class ImageGallery extends React.Component{
-  state={
-        fotos:[],
-        currentPage: 1,
-        loading: false,
-        error: null,
-        showModal: false,
-        largeImage: '',
-  }   
- 
-  async componentDidUpdate(prevProps, prevState){
-        const nameSearch =this.props.nameSearch
-        const prevName = prevProps.nameSearch
-        if(prevName!==nameSearch||this.state.currentPage!==prevState.currentPage){
-                this.getFoto();     
-        }
-      } 
-  getFoto = async () =>{
-    const nameSearch =this.props.nameSearch
-    this.setState({loading: true})
+export default class ImageGallery extends React.Component {
+  state = {
+    fotos: [],
+    currentPage: 1,
+    loading: false,
+    error: null,
+    showModal: false,
+    largeImage: '',
+  };
+
+  async componentDidUpdate(prevProps, prevState) {
+    const nameSearch = this.props.nameSearch;
+    const prevName = prevProps.nameSearch;
+    if (
+      prevName !== nameSearch ||
+      this.state.currentPage !== prevState.currentPage
+    ) {
+      this.getFoto();
+    }
+  }
+  getFoto = async () => {
+    const nameSearch = this.props.nameSearch;
+    this.setState({ loading: true });
     try {
-      const {hits} = await fetchFoto(nameSearch, this.state.currentPage)
+      const { hits } = await fetchFoto(nameSearch, this.state.currentPage);
       this.setState(prevState => ({
         fotos: [...prevState.fotos, ...hits],
-              }));          
-            } catch (error) {
-              this.setState({ error });
-            }finally{
-            this.setState({loading:false})
-            }
-            }
+      }));
+    } catch (error) {
+      this.setState({ error });
+    } finally {
+      this.setState({ loading: false });
+    }
+  };
 
-  loadFoto = ()=>{
-    this.setState(prevState =>({currentPage:prevState.currentPage+=1}))   
-  }
+  loadFoto = () => {
+    this.setState(prevState => ({ currentPage: (prevState.currentPage += 1) }));
+  };
 
-  handleGalleryItem = (largeImageSrc) => {   
-        this.setState({        
-          largeImage: largeImageSrc,
-          showModal: true,
-        });     
-      };
- 
+  handleGalleryItem = largeImageSrc => {
+    this.setState({
+      largeImage: largeImageSrc,
+      showModal: true,
+    });
+  };
+
   toggleModal = () => {
-            this.setState(prevState => ({
-              showModal: !prevState.showModal,
-              largeImage: '',
-            }));
-          };
+    this.setState(prevState => ({
+      showModal: !prevState.showModal,
+      largeImage: '',
+    }));
+  };
 
-        render(){
-            const {fotos, loading, error, showModal, largeImage } = this.state;
-            const needToShowLoadMore = (fotos.length >= this.state.currentPage*12);          
-        return(
-          <div> 
-           {showModal&& <Modal onClose ={this.toggleModal}>
-           <img src={largeImage} alt="" />
-           </Modal> }    
-          <ul className="ImageGallery">
-           {fotos ? ( <ImageGalleryItem items={fotos} onImageClick={this.handleGalleryItem} />) : (<div></div>)}
-          </ul>
-           {needToShowLoadMore?( <Button onLoadFoto ={this.loadFoto}/>):<div></div>}
-           {loading&&<Loader/>}
-           {error&&<h1>Error...</h1>} 
-          </div>       
-          )} 
+  render() {
+    const { fotos, loading, error, showModal, largeImage } = this.state;
+    const needToShowLoadMore = fotos.length >= this.state.currentPage * 12;
+    console.log(needToShowLoadMore);
+    return (
+      <div>
+        {showModal && (
+          <Modal onClose={this.toggleModal}>
+            <img src={largeImage} alt="" />
+          </Modal>
+        )}
+        <ul className="ImageGallery">
+          {fotos ? (
+            <ImageGalleryItem
+              items={fotos}
+              onImageClick={this.handleGalleryItem}
+            />
+          ) : (
+            <div></div>
+          )}
+        </ul>
+        {needToShowLoadMore ? (
+          <Button onLoadFoto={() => this.loadFoto} />
+        ) : (
+          false
+        )}
+        {loading && <Loader />}
+        {error && <h1>Error...</h1>}
+      </div>
+    );
+  }
 }
